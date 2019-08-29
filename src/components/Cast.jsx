98f345288f395debc5b0f500cabe438115044e63
Adam Jones <adam@linuxtampa.com>
2019-08-29 16:23:51 -0400

    added cast .jsx

---
+++ src/components/Cast.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Cast = () => {
+  const [cast, setCast] = useState([])
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/tv/1/credits?api_key=785c5d04247a97014c1e2374403ebdc2&language=en-US`
+    )
+    console.log(response.data.cast)
+    setCast(response.data.cast)
+    console.log({ cast })
+  }
+
+  useEffect(() => {
+    fetchData()
+    console.log({ cast })
+  }, [])
+
+  return (
+    <p>
+      {cast.map(member => {
+        return <span>{member.character}</span>
+      })}
+    </p>
+  )
+}
+
+export default Cast
