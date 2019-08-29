d7f2f4bb1d0c94f9a3aef69d316cd7deaac65902
Mallory Smith <mallorysmith6464@gmail.com>
2019-08-29 15:02:49 -0400

    working on rendering movies

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,23 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-//async cannot go on components
-
 const HomePage = () => {
-  const fetchData = async () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  //async cannot go on components
+  const getTopMovies = async () => {
+    // const fetchData = async () => {
     const resp = await axios.get(
       `https://api.themoviedb.org/3/tv/top_rated?api_key=c2cccc08a093d5b7cfe606b5745c1af5&language=en-US&page=1`
     )
-    console.log('setup api', resp.data)
+    console.log(resp.data)
+    // console.log('list of all top rated TV shows', resp.data.results)
+    // setTopMovies(resp.data)
   }
 
   useEffect(() => {
-    fetchData()
+    getTopMovies()
   }, [])
 
   return (
     <>
-      <h1>something here</h1>
+      {topMovies.map((movie, i) => {
+        return <h3 key={i}>{movie.movie}</h3>
+      })}
     </>
   )
 }
