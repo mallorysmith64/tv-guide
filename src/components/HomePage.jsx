2d3b2f2037fbe64842f788df0f5b82e683aa572f
Mallory Smith <mallorysmith6464@gmail.com>
2019-08-30 21:10:41 -0400

    all movie titles are rendered

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const HomePage = () => {
-  const [topMovies, setTopMovies] = useState([])
+  const [topMovies, setTopMovies] = useState([]) //array in console
 
   //async cannot go on components
   const getTopMovies = async () => {
@@ -10,9 +10,10 @@ const HomePage = () => {
     const resp = await axios.get(
       `https://api.themoviedb.org/3/tv/top_rated?api_key=c2cccc08a093d5b7cfe606b5745c1af5&language=en-US&page=1`
     )
-    console.log(resp.data)
-    // console.log('list of all top rated TV shows', resp.data.results)
-    // setTopMovies(resp.data)
+    console.log('got all top movies', resp.data)
+    console.log('targeting one movie', resp.data.results[0])
+    console.log('targeting one movie title:', resp.data.results[0].name)
+    setTopMovies(resp.data.results)
   }
 
   useEffect(() => {
@@ -22,7 +23,11 @@ const HomePage = () => {
   return (
     <>
       {topMovies.map((movie, i) => {
-        return <h3 key={i}>{movie.results}</h3>
+        return (
+          <section key={i}>
+            <h3>{movie.original_name}</h3>
+          </section>
+        )
       })}
     </>
   )
