2f9be0fd8f79d296ac2d3976b144a54008d490eb
Mallory Smith <mallorysmith6464@gmail.com>
2019-08-31 22:03:45 -0400

    random photo renders

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Movie from './Movie'
 
 const HomePage = () => {
   const [topMovies, setTopMovies] = useState([])
   const imgSize = 'w200'
   // const [randomIndex, setRandomIndex] = useState(0)
-  const imageUrl = 'https://image.tmdb.org/t/p/'
+  // const imageUrl = 'https://image.tmdb.org/t/p/'
   const [randomMovie, setRandomMovie] = useState({})
 
   const getTopMovies = async () => {
@@ -21,13 +21,11 @@ const HomePage = () => {
   }
 
   const getRandomPicture = photos => {
-    console.log(photos)
     let randomIdx = Math.floor(Math.random() * photos.length)
     console.log(randomIdx)
     let randMovie = photos[randomIdx]
-    console.log(randMovie)
+    console.log('random movie', randMovie.original_name)
     setRandomMovie(randMovie)
-    // return randomPhotos
   }
 
   useEffect(() => {
@@ -38,24 +36,15 @@ const HomePage = () => {
 
   return (
     <>
-      {/* <h1>Random Movie</h1>
-      <section className="random-photo"> */}
-      {/* <h1>{topMovies[randomIndex].original_name}</h1> */}
-      {/* <img
-          src={`${imageUrl}${imgSize}${topMovies[randomIndex].poster_path}`}
-          alt={topMovies[randomIndex].id}
-        /> */}
-      {/* </section> */}
+      <Movie className="random-photo" movie={randomMovie} imgSize={imgSize} />
       <h1>Top Rated Movies</h1>
       <Switch>
         <Route exact path="/">
-          <h2>Top Rated Movies</h2>
           {topMovies.map((movie, i) => {
             return <Movie movie={movie} imgSize={imgSize} key={i} />
           })}
         </Route>
         <Route path="/a">
-          {console.log('topMovies', topMovies)}
           <Movie movie={topMovies[0]} imgSize={imgSize} />
         </Route>
       </Switch>
