27dedccf4d55d8e848851c91213c851d74c323b2
Adam Jones <adam@linuxtampa.com>
2019-09-01 23:57:59 -0400

    warning cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import HomePage from './components/HomePage.jsx'
-import Cast from './components/Cast'
 
 class App extends Component {
   render() {
