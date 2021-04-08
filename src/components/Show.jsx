03f7c2f7bf12cafd2e81b85d937aaccaec58c1cc
mallorysmith64 <mallorysmith64@gmail.com>
2021-04-07 21:08:01 -0400

    updated dependencies, utilizing react-show-more-text for truncating descriptions

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,34 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Cast from "../components/Cast";
+import ShowMoreText from "react-show-more-text"; // truncate show descriptions
 
 const Show = props => {
   const { show, imgSize } = props;
   const imageUrl = "https://image.tmdb.org/t/p/";
-  // const maxLength = 300;
-
-  // const { text } = props;
 
   if (!show) return <></>;
 
+  const executeOnClick = isExpanded => {
+    console.log(isExpanded);
+  };
+
   return (
     <div className={`${!props.showCast ? "show-multi" : ""}`}>
-      {/* {text.length > maxLength ? (
-        <div className={`${text.substring(0, maxLength)}...`}>
-          <a href="/#">Read more</a>
-        </div>
-      ) : (
-        <p>{text}</p>
-      )} */}
-
       <section className="green-background show-picked-container">
-        <section className="show-picked">
+        <section className="show-picked text">
           <h3>
             Title: <Link to={`/show/${show.id}`}>{show.original_name}</Link>
           </h3>
           <h4> First Aired: {show.first_air_date}</h4>
           <img src={`${imageUrl}${imgSize}${show.poster_path}`} alt={show.id} />
-          <h4 className="text"> Description: {show.overview}</h4>
+
+          <ShowMoreText
+            /* Default options */
+            lines={3}
+            more="Show more"
+            less="Show less"
+            className="content-css"
+            anchorClass="my-anchor-css-class"
+            onClick={executeOnClick}
+            expanded={false}
+            width={290}
+          >
+            <h4> Description: {show.overview}</h4>
+          </ShowMoreText>
         </section>
       </section>
 
@@ -39,4 +46,5 @@ const Show = props => {
     </div>
   );
 };
+
 export default Show;
